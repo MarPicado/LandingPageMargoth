d75f833a8814479b65d68dbb3b0e202f2607d3e9
Margoth <mloreana@gmail.com>
2021-03-03 18:52:52 +0000

    exercise landing page - better look, no errors

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,40 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export function Card() {
-    return (
-        <div className="card-group">
-            <div className="card" style="width: 18rem;">
-                <img className="card-img-top" src="..." alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div className="card" style="width: 18rem;">
-                <img className="card-img-top" src="..." alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div className="card" style="width: 18rem;">
-                <img className="card-img-top" src="..." alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div className="card" style="width: 18rem;">
-                <img className="card-img-top" src="..." alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    );
+export function Card(props) {
+	return (
+		<div className="card" style={{ width: "18rem" }}>
+			<img
+				className="card-img-top"
+				src={props.img}
+				alt="Card image cap"
+			/>
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">{props.body}</p>
+				<a href={props.link} className={"btn btn-" + props.buttoncolor}>
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
 }
+
+Card.propTypes = {
+	img: PropTypes.string,
+	title: PropTypes.string,
+	body: PropTypes.string,
+	link: PropTypes.string,
+	buttoncolor: PropTypes.string
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,11 +7,40 @@ import { Card } from "./card";
 
 //create your first component
 export function Home() {
-    return (
-        <div className="text-center mt-5">
-            <Navbar />
-            <Jumbotron />
-            <Card />
-        </div>
-    );
+	return (
+		<div className="text-center mt-5">
+			<Navbar />
+			<Jumbotron />
+			<div className="d-flex justify-content-center">
+				<Card
+					title={"Title #1"}
+					body={"Body card"}
+					img={"https://randomuser.me/api/portraits/women/24.jpg"}
+					link={"http://www.google.com"}
+					buttoncolor={"primary"}
+				/>
+				<Card
+					title={"Title #2"}
+					body={"Body card2"}
+					img={"https://randomuser.me/api/portraits/women/25.jpg"}
+					link={"http://www.google.com"}
+					buttoncolor={"success"}
+				/>
+				<Card
+					title={"Title #3"}
+					body={"Body card3"}
+					img={"https://randomuser.me/api/portraits/women/24.jpg"}
+					link={"http://www.google.com"}
+					buttoncolor={"danger"}
+				/>
+				<Card
+					title={"Title #4"}
+					body={"Body card4"}
+					img={"https://randomuser.me/api/portraits/women/25.jpg"}
+					link={"http://www.google.com"}
+					buttoncolor={"secondary"}
+				/>
+			</div>
+		</div>
+	);
 }
