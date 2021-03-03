748c635d8a59d3b85f81166564ce1a254d5c3022
Margoth <mloreana@gmail.com>
2021-03-03 19:54:32 +0000

    landing page best version

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -15,28 +15,36 @@ export function Home() {
 				<Card
 					title={"Title #1"}
 					body={"Body card"}
-					img={"https://randomuser.me/api/portraits/women/24.jpg"}
+					img={
+						"https://www.splendidbeast.com/wp-content/uploads/2016/02/The-Napoleon-Large.png"
+					}
 					link={"http://www.google.com"}
 					buttoncolor={"primary"}
 				/>
 				<Card
 					title={"Title #2"}
 					body={"Body card2"}
-					img={"https://randomuser.me/api/portraits/women/25.jpg"}
+					img={
+						"https://www.giveamasterpiece.com/images/funny-dog-portrait-oil-GAM2974_1000.jpg"
+					}
 					link={"http://www.google.com"}
 					buttoncolor={"success"}
 				/>
 				<Card
 					title={"Title #3"}
 					body={"Body card3"}
-					img={"https://randomuser.me/api/portraits/women/24.jpg"}
+					img={
+						"https://d2j6dbq0eux0bg.cloudfront.net/images/12162034/1198649352.jpg"
+					}
 					link={"http://www.google.com"}
 					buttoncolor={"danger"}
 				/>
 				<Card
 					title={"Title #4"}
 					body={"Body card4"}
-					img={"https://randomuser.me/api/portraits/women/25.jpg"}
+					img={
+						"https://www.visualchaos.co.uk/wp-content/uploads/2020/03/Edward1-sq.jpg"
+					}
 					link={"http://www.google.com"}
 					buttoncolor={"secondary"}
 				/>
