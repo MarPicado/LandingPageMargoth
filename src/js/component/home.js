6cd3296517e92a8b2ecd1bc13ce1821a57ac68d1
Margoth <mloreana@gmail.com>
2021-03-03 04:06:24 +0000

    Landing Page

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,13 +3,15 @@ import React from "react";
 //include images into your bundle
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
+import { Card } from "./card";
 
 //create your first component
 export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<Navbar />
-            <Jumbotron/>
-		</div>
-	);
+    return (
+        <div className="text-center mt-5">
+            <Navbar />
+            <Jumbotron />
+            <Card />
+        </div>
+    );
 }
